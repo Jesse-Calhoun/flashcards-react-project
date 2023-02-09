105f1477494f1c2b7dd0a4658c041d92b7e7944d
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-09 12:24:26 -0500

    Built out the Card, CardContainer and CardViewer components, to realize I was not having my data be returned from the backend and database to my frontend, I then commented out the imports of all the components in the App function and other code, to build it out from scratch and ensure my data was beign transferred.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Sidebar from './Components/Sidebar/Sidebar';
-import Header from './Components/Header/Header';
+// import Sidebar from './Components/Sidebar/Sidebar';
+// import Header from './Components/Header/Header';
+// import CardContainer from './Components/CardContainer/CardContainer';
 
 
 function App() {
 
   const [collections,setCollections] = useState([])
-  const [cards, setCards] = useState([])
+  // const [cards, setCards] = useState([])
 
   useEffect(() => {
     getAllCollections();
   }, [])
 
-  useEffect(() => {
-    getCardsInCollection();
-  }, [cards])
+  // useEffect(() => {
+  //   getCardsInCollection();
+  // }, [])
 
   async function getAllCollections(){
-    let response = await axios.get("http://127.0.0.1:8000/api/collections/");
+    const response = await axios.get("http://127.0.0.1:8000/api/collections/");
     setCollections(response.data);
   }
 
   
-  async function getCardsInCollection(collection) {
-    let url = "http://127.0.0.1:8000/api/collections/" + collection.id + "/cards/";
-    let response = await axios.get(url);
-    setCards(response.data)
-  }
- 
+  // async function getCardsInCollection(collection) {
+  //   collection.id = 2
+  //   let url = "http://127.0.0.1:8000/api/collections/" + collection.id + "/cards/";
+  //   const response = await axios.get(url);
+  //   setCards(response.data)
+  // }
+
+  // getAllCollections()
+  console.log(collections)
+  // console.log(cards) 
 
   return (
-    <div>
-      <Header/>
-      <Sidebar collections={collections} getCardsInCollection={getCardsInCollection} />
+    <div id='app'>
+      <h3>Collections</h3>
+      {/* <button onClick={getCardsInCollection}>Flutter</button> */}
+      {/* <Header/>
+      <Sidebar collections={collections} getCardsInCollection={getCardsInCollection} getAllCollections={getAllCollections} />
+      <CardContainer cards={cards}/> */}
     </div>
   );
 }
